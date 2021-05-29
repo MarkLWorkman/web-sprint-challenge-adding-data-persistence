4c444aaa31498abbceaf00de4fb56dd474ad2ac1
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-28 22:46:24 -0400

    api/resources complete

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,15 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+const findResources = () => {
+  return db("resources");
+};
+
+const insertResource = async (newResource) => {
+  const [resource_id] = await db("resources").insert(newResource);
+  return db("resources").where({ resource_id }).first();
+};
+
+module.exports = {
+  insertResource,
+  findResources,
+};
