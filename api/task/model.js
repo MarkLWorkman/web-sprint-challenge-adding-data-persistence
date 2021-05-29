966adc178b343a519bb497eb25c8d5779c0cdb66
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-28 22:47:53 -0400

    api/tasks complete

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,57 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+const intToBoolean = (int) => {
+  return int === 1 ? true : false;
+};
+
+function taskToBody(task) {
+  return {
+    ...task,
+    task_completed: intToBoolean(task.task_completed),
+  };
+}
+
+const findTasks = () => {
+  return db("tasks as t")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    )
+    .join("projects as p", "p.project_id", "t.project_id")
+    .orderBy("p.project_name")
+    .then((tasks) => {
+      return tasks.map((task) => taskToBody(task));
+    });
+};
+
+const insertTasks = async (newTask) => {
+  return db("tasks as t")
+    .insert(newTask, "task_id")
+    .then(([task_id]) => findById(task_id));
+};
+
+const findById = async (task_id) => {
+  return db("tasks as t")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_id"
+    )
+    .join("projects as p", "p.project_id", "t.project_id")
+    .orderBy("p.project_id")
+    .where({ task_id })
+    .then((tasks) => {
+      return tasks.map((task) => taskToBody(task));
+    });
+};
+
+module.exports = {
+  findTasks,
+  insertTasks,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,27 @@
-// build your `/api/tasks` router here
+const router = require("express").Router();
+
+const Tasks = require("./model");
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Tasks.findTasks();
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    if (typeof req.body.task_completed !== "boolean") {
+      res.status(400).json({ message: "task_completed must be a boolean" });
+    } else {
+      const newTasks = await Tasks.insertTasks(req.body);
+      res.status(201).json(newTasks);
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
