ba6612b3dbbfb9b059187633bcd0afa54a5aafb3
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-28 22:23:12 -0400

    seeds complete

--- data/seeds/02-resource.js
+++ data/seeds/02-resource.js
@@ -1,13 +1,14 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+  return knex("resources").insert([
+    {
+      resource_id: 1,
+      resource_name: "Wood",
+      resource_description: "Test test test test",
+    },
+    {
+      resource_id: 2,
+      resource_name: "Brick",
+      resource_description: "Banana banana banana",
+    },
+  ]);
 };
--- data/seeds/03-task.js
+++ data/seeds/03-task.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function (knex) {
+  return knex("tasks").insert([
+    {
+      task_id: 1,
+      task_description: "this is a test",
+      task_notes: "yes i love tests",
+      task_completed: true,
+      project_id: 1,
+    },
+  ]);
 };
