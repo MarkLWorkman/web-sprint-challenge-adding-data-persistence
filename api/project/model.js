7172a651dcff085721ad44533ff9f233a87a618b
MarkLWorkman <70355546+MarkLWorkman@users.noreply.github.com>
2021-05-28 22:44:46 -0400

    api/projects complete

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,37 @@
-// build your `Project` model here
+const db = require("../../data/dbConfig");
+
+const intToBoolean = (int) => {
+  return int === 1 ? true : false;
+};
+
+function projectToBody(project) {
+  return {
+    ...project,
+    project_completed: intToBoolean(project.project_completed),
+  };
+}
+
+const findProjects = () => {
+  return db("projects").then((projects) => {
+    return projects.map((project) => projectToBody(project));
+  });
+};
+
+const findById = (project_id) => {
+  return db("projects")
+    .where({ project_id })
+    .then((projects) => {
+      return projects.map((project) => projectToBody(project));
+    });
+};
+
+const insertProject = async (newProject) => {
+  return db("projects as p")
+    .insert(newProject, "project_id")
+    .then(([project_id]) => findById(project_id));
+};
+
+module.exports = {
+  findProjects,
+  insertProject,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,27 @@
-// build your `/api/projects` router here
+const router = require("express").Router();
+
+const Projects = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Projects.findProjects();
+    res.status(200).json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    if (typeof req.body.project_completed !== "boolean") {
+      res.status(400).json({ message: "project_completed must be a boolean" });
+    } else {
+      const newProject = await Projects.insertProject(req.body);
+      res.status(201).json(newProject);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
